8765f55490f0b13d9687fb75b0936f11f2d9cbbb
Alex Vedmidskiy <gagaga1024@gmail.com>
2016-09-24 13:29:19 +0200

    solved MiaximumSubarraySum with AVL tree

--- hackerrank.com/maximum-subarray-sum/main.cpp
+++ hackerrank.com/maximum-subarray-sum/main.cpp
@@ -4,67 +4,111 @@ using namespace std;
 #define COUT if(0) cout <<
 #define FOR(i,n) for(int i=0;i<n;i++)
 #define FORBE(t,i,b,e) for(t* i=b;i<e;i++)
-#define MAX(a,b) (a)>(b)?(a):(b)
+#define MAX(a,b) ((a)>(b)?(a):(b))
 
 static const int NMAX = 100001;
 typedef unsigned long long int ui64;
 typedef long long int T;
 
-template <class T>
-struct tNode
-{
-    T value;
-    tNode* l;
-    tNode* r;
-    tNode(): l(nullptr), r(nullptr) {}
-};
-
 template<class T, int S>
-struct BinaryTree {
-    tNode<T> nodes[S];
-    tNode<T>* root;
-    int size;
-    int depth;
-    BinaryTree(): root(nullptr), size(0), depth(0) {}
-    tNode<T>* push(T val) {
-        tNode<T>* node = &nodes[size++];
-        node->value = val;
-        if (root == nullptr) {
-            root = node;
-            depth = 1;
+struct AvlTree {
+    struct Node {
+        static Node* getnull() {
+            static Node null;
+            return &null;
         }
-        else {
-            tNode<T>* parent = root;
-            int nd = 2;
-            for (;; nd++) {
-                tNode<T>*& candidate = (val <= parent->value) ? parent->l : parent->r;
-                if (candidate == nullptr) {
-                    candidate = node;
-                    break;
-                }
-                parent = candidate;
-            }
-            if (nd > depth) depth = nd;
+        inline bool isnull() { return this == getnull(); }
+        T value;
+        int height = -1;
+        Node* kids[2] = { nullptr, nullptr };
+        Node(const T& v): value(v), height(0) {
+            kids[0] = getnull();
+            kids[1] = getnull();
         }
-        return node;
+        Node() {}
+        Node(const Node&) = default;
+        Node& operator= (const Node&) = default;
+    };
+
+    Node nodes[S];
+    Node* root = Node::getnull();
+    int size = 0;
+
+    Node* insert(T val) {
+        return insert(val, root);
     }
-    tNode<T>* ceil(T val) {
-        tNode<T>* node = root;
-        tNode<T>* last = nullptr;
-        while (node) {
-            if (val == node->value) return node;
-            if (val <  node->value) {
+
+    Node* insert(T val, Node*& node) {
+        if (node->isnull()) {
+            node = &nodes[size++];
+            *node = Node(val);
+            return node;
+        }
+        Node*& kid = node->kids[val > node->value];
+        Node* inserted = insert(val, kid);
+        node->height = MAX(node->height, kid->height + 1);
+        node = balance(node);
+        return inserted;
+    }
+
+    Node* rotate(Node* node, int dir) {
+        node->height -= 2;
+        Node* kid = node->kids[dir];
+        node->kids[dir] = kid->kids[!dir];
+        kid->kids[!dir] = node;
+        return kid;
+    }
+
+    // true if right kid is heavier, false otherwise
+    bool bias(Node* node) {
+        return (node->kids[0]->height - node->kids[1]->height < 0);
+    }
+
+    Node* balance(Node* node) {
+        if (node->height < 2) return node;
+        // right goes negative
+        int bal = node->kids[0]->height - node->kids[1]->height;
+        if (bal >= -1 && bal <=1 ) return node;
+        bool dir = bias(node);
+        Node* heavy_kid = node->kids[dir];
+        if (bias(heavy_kid) != dir) {
+            node->kids[dir] = rotate(heavy_kid, !dir);
+        }
+        return rotate(node, dir);
+    }
+
+    Node* find(T val, Node* node) {
+        if (node->isnull()) return nullptr;
+        if (node->value > val) return node;
+        return find(val, node->kids[val > node->value]);
+    }
+
+    Node* ceil(T val) {
+        Node* node = root;
+        Node* last = nullptr;
+        while (!node->isnull()) {
+            if (node->value == val) return node;
+            if (node->value < val) node = node->kids[1];
+            else {
                 last = node;
-                node = node->l;
-            }   
-            else node = node->r;
+                node = node->kids[0];
+            }
         }
         return last;
     }
+
+    template<class F> void iterate(F& func) { iterate(func, root); }
+    template<class F> bool iterate(F& func, Node* node) {
+        if (node->isnull()) return true;
+        if (iterate(func, node->kids[0]) == false) return false;
+        if (func(node) == false) return false;
+        return iterate(func, node->kids[1]);
+    }
 };
 
+
 T test() {
-    BinaryTree<T, NMAX> array;
+    AvlTree<T, NMAX> array;
     T n, m, val, sum = 0, result = 0;
     cin >> n >> m;
     COUT "    ==== n: " << n << "; m: " << m << " ====" << endl;
@@ -74,7 +118,7 @@ T test() {
         val %= m;
         sum = (sum + val) % m;
         COUT "*** val: " << val << "; sum: " << sum;
-        tNode<T>* ceil = array.ceil(sum + 1);
+        auto ceil = array.ceil(sum + 1);
         if (ceil) {
             T diff = (sum + m - ceil->value);
             if (diff > result) result = diff;
@@ -82,9 +126,9 @@ T test() {
         }
         else if (sum > result) result = sum;
         COUT "; result: " << result << endl;
-        array.push(sum);
+        array.insert(sum);
     }
-    cout << "Tree depth: " << array.depth << endl;
+    //cout << "Tree depth: " << array.depth << endl;
     return result;
 }
 
