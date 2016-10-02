3f3eb0de131cb9426249e520623fd0f3d50e7eb1
Alex Vedmidskiy <gagaga1024@gmail.com>
2016-10-02 08:34:36 +0200

    Optimized O(N) solutions for https://www.hackerrank.com/challenges/non-divisible-subset

--- hackerrank.com/non-divisible-subset/main.cpp
+++ hackerrank.com/non-divisible-subset/main.cpp
@@ -1,53 +1,19 @@
 #include <stdio.h>
 
-#ifdef _DEBUG
-#define DBG(...) printf(__VA_ARGS__)
-#else
-#define DBG(...)
-#endif
-
 #define MAX(a,b) ((a)>(b)?(a):(b))
 
 #define KMAX 100 
 
-static int rems[KMAX];
-static int* rems_sorted[KMAX];
-int N;
-int k;
-
-template <class T>
-inline void swap(T& a, T& b) {
-    T t = a; a = b; b = t;
-}
-
-void qsort(int** data, int n)
-{
-    if (n < 2) return;
-    int p = *data[n/2];
-    int i = 0, j = n - 1;
-    for(;; i++, j--) {
-        while (*data[i] < p) i++;
-        while (*data[j] > p) j--;
-        if (i >= j) break;
-        swap(data[i], data[j]);
-    }
-    qsort(data, i);
-    qsort(data + i, n-i);
-}
-
 int main()
 {
+    static int rems[KMAX];
+    int N, k, a;
+
     scanf("%d%d", &N, &k);
-    for (int i = 0; i < k; i++) {
-        rems_sorted[i] = &rems[i];
-    }
-    int a;
-    for (int i = 0; i < N; i++) {
+    while (N--) {
         scanf("%d", &a);
-        DBG("%d ", a%k);
         rems[a % k]++;
     }
-    DBG("\n");
 
     int result = (rems[0] > 0);
     rems[0] = 0;
@@ -56,19 +22,8 @@ int main()
         rems[k / 2] = 0;
     }
 
-    for (int i = 0; i < k; i++) DBG("[%lu] %d, ", rems_sorted[i] - rems, *rems_sorted[i]);
-    DBG("\n");
-    qsort(rems_sorted, k);
-    for (int i = 0; i < k; i++) DBG("[%lu] %d, ", rems_sorted[i] - rems, *rems_sorted[i]);
-    DBG("\n");
-
-    for (int i = k - 1; i >= 0; i--) {
-        int irem = rems_sorted[i] - rems;
-        if (rems[irem] == 0) continue;
-        result += rems[irem];
-        rems[k - irem] = 0;
-        DBG("Check remainder %d: met %d times, zero remainder %d; result %d\n",
-                irem, *rems_sorted[i], k - irem, result);
+    for (int i = 1, j = k - 1; i < j; i++, j--) {
+        result += MAX(rems[i], rems[j]);
     }
 
     printf("%d", result);
