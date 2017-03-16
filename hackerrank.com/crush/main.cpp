60c3ce35f068fcb945a4b388d0737a6b3119512f
Alex Vedmidskiy <gagaga1024@gmail.com>
2017-03-16 21:34:46 +0100

    tabs

--- hackerrank.com/crush/main.cpp
+++ hackerrank.com/crush/main.cpp
@@ -14,11 +14,11 @@ static i64 ar[MAXN];
 int main()
 {
 	int N, M;
-    scanf("%d %d", &N, &M);
+	scanf("%d %d", &N, &M);
 
 	for (int i = 0; i < M; i++) {
 		int a, b, k;
-        scanf("%d %d %d", &a, &b, &k);
+		scanf("%d %d %d", &a, &b, &k);
 		ar[a] += k;
 		ar[b+1] -= k;
 	}
@@ -29,6 +29,6 @@ int main()
 		cur += ar[i];
 		if (cur > max) max = cur;
 	}
-    printf("%lld\n", max);
+	printf("%lld\n", max);
 	return 0;
 }
